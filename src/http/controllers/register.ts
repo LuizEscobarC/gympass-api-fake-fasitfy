0c24aa5270460e5d55836b94eedcba610d06e2e0
LuizEscobarC <luiz_escobar11@hotmail.com>
2025-02-08 16:13:48 -0400

    feat: bcryptjs and validate email

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
+import { hash } from 'bcryptjs'
 
 export  async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -11,12 +12,24 @@ export  async function register(request: FastifyRequest, reply: FastifyReply) {
   })
   
   const { name, email, password } = registerBodySchema.parse(request.body)
+
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email
+    }
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(400).send()
+  }
   
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password
+      password_hash
     }
   })
   
